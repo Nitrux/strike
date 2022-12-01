9c24787f8ea50e1a5ffe912497d7af20e9dbc344
Uri Herrera <hello@uriherrera.com>
2022-11-30 23:45:05 -0600

    update pkg

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # Strike
+![](https://mauikit.org/wp-content/uploads/2018/12/maui_project_logo.png)
 
-Strike is a simple minimal IDE for the Linux phones. Code, build, and run from the phone.
+[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0) [![Awesome](https://awesome.re/badge.svg)](https://awesome.re) [![Generic badge](https://img.shields.io/badge/OS-Linux-blue.svg)](https://shields.io/)
+
+_Strike IDE is a C++ and CMake-based application to build projects._
+
+# Screenshots
+
+![station](https://mauikit.org/wp-content/uploads/2022/11/strike.jpg)
 
 # Issues
 If you find problems with the contents of this repository please create an issue.
 
-©2022 Nitrux Latinoamericana S.C.
+©2021 Nitrux Latinoamericana S.C.
--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -4,24 +4,19 @@ set -x
 
 ### Update sources
 
-wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.user
+wget -qO /etc/apt/sources.list.d/nitrux-main-compat-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources/nitrux-repo.list
 
-wget -qO /etc/apt/sources.list.d/nitrux-main-compat-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux
-
-wget -qO /etc/apt/sources.list.d/nitrux-testing-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux.testing
-
-DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
-	55751E5D > /dev/null
+wget -qO /etc/apt/sources.list.d/nitrux-testing-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources/nitrux-testing-repo.list
 
 curl -L https://packagecloud.io/nitrux/repo/gpgkey | apt-key add -;
 curl -L https://packagecloud.io/nitrux/compat/gpgkey | apt-key add -;
 curl -L https://packagecloud.io/nitrux/testing/gpgkey | apt-key add -;
 
-DEBIAN_FRONTEND=noninteractive apt -qq update
+apt -qq update
 
 ### Install Package Build Dependencies #2
 
-DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
+apt -qq -yy install --no-install-recommends \
 	mauikit-git \
 	mauikit-filebrowsing-git
 
@@ -48,6 +43,8 @@ cmake \
 
 make -j$(nproc)
 
+make install
+
 ### Run checkinstall and Build Debian Package
 
 >> description-pak printf "%s\n" \
@@ -72,7 +69,7 @@ checkinstall -D -y \
 	--pakdir=. \
 	--maintainer=uri_herrera@nxos.org \
 	--provides=strike \
-	--requires="libc6,libgcc-s1,libkf5coreaddons5,libkf5i18n5,libqt5core5a,libqt5gui5,libqt5qml5,libqt5quick5,libqt5widgets5,libstdc++6,mauikit-git \(\>= 2.2.0+git\),mauikit-filebrowsing-git \(\>= 2.2.0+git\)" \
+	--requires="libc6,libgcc-s1,libkf5coreaddons5,libkf5i18n5,libqt5core5a,libqt5gui5,libqt5qml5,libqt5quick5,libqt5widgets5,libstdc++6,mauikit-git \(\>= 2.2.1+git\),mauikit-filebrowsing-git \(\>= 2.2.1+git\)" \
 	--nodoc \
 	--strip=no \
 	--stripso=yes \
